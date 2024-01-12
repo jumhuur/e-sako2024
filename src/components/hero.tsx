ef07d037c28227b970815f61db03a1ad4436e3bd
abu-amar jumhuur <jumhuur123@hotmail.com>
2024-01-12 18:32:25 +0300

    done

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,6 +4,50 @@ const Hero = () => {
   return (
     <>
       <div className="hero">
+        <Image
+          className="her_icons kaw"
+          src={"/images/xagal2.png"}
+          width={40}
+          height={40}
+          alt="icons"
+        />
+        <Image
+          className="her_icons laba"
+          src={"/images/xagal2.png"}
+          width={40}
+          height={40}
+          alt="icons"
+        />
+
+        <Image
+          className="her_icons sadex"
+          src={"/images/o.png"}
+          width={40}
+          height={40}
+          alt="icons"
+        />
+        <Image
+          className="her_icons afar"
+          src={"/images/o.png"}
+          width={40}
+          height={40}
+          alt="icons"
+        />
+        <Image
+          className="her_icons shan"
+          src={"/images/line.png"}
+          width={40}
+          height={40}
+          alt="icons"
+        />
+
+        <Image
+          className="her_icons lix"
+          src={"/images/x2.png"}
+          width={40}
+          height={40}
+          alt="icons"
+        />
         <div className="text_hero">
           <h2>Ku xisaabso E-zako Sakadada Si Waafaqsan Shareecada</h2>
           <p>
@@ -14,50 +58,6 @@ const Hero = () => {
         <button type="button">
           <i className="fa-solid fa-square-root-variable"></i> Xisaabi Sako
         </button>
-        <Image
-          className="her_icons"
-          src={"/images/xagal2.png"}
-          width={40}
-          height={40}
-          alt="icons"
-        />
-        <Image
-          className="her_icons"
-          src={"/images/xagal2.png"}
-          width={40}
-          height={40}
-          alt="icons"
-        />
-
-        <Image
-          className="her_icons"
-          src={"/images/o.png"}
-          width={40}
-          height={40}
-          alt="icons"
-        />
-        <Image
-          className="her_icons"
-          src={"/images/o.png"}
-          width={40}
-          height={40}
-          alt="icons"
-        />
-        <Image
-          className="her_icons"
-          src={"/images/line.png"}
-          width={40}
-          height={40}
-          alt="icons"
-        />
-
-        <Image
-          className="her_icons"
-          src={"/images/x2.png"}
-          width={40}
-          height={40}
-          alt="icons"
-        />
       </div>
     </>
   );
--- src/components/rate.tsx
+++ src/components/rate.tsx
@@ -1,29 +1,26 @@
+"use client";
 import Image from "next/image";
-
+import { useState } from "react";
 const Rate = () => {
+  const [rate, setrate] = useState(false);
+  const [loading, setloading] = useState(false);
+  const Ratechange = (): boolean => {
+    setrate(true);
+    return rate;
+  };
   return (
     <>
       <div className="home">
         <div className="hero">
-          <div className="text_hero">
-            <h2 className="rate_title">
-              Qiimee shabakada Esako
-              <br />
-              Jwaabaheeda & qaabkeeda
-            </h2>
-          </div>
-          <button type="button">
-            <i className="fa-solid fa-star-half-stroke"></i> Qiimee
-          </button>
           <Image
-            className="her_icons"
+            className="her_icons kaw"
             src={"/images/xagal2.png"}
             width={40}
             height={40}
             alt="icons"
           />
           <Image
-            className="her_icons"
+            className="her_icons laba"
             src={"/images/xagal2.png"}
             width={40}
             height={40}
@@ -31,21 +28,21 @@ const Rate = () => {
           />
 
           <Image
-            className="her_icons"
+            className="her_icons sadex"
             src={"/images/o.png"}
             width={40}
             height={40}
             alt="icons"
           />
           <Image
-            className="her_icons"
+            className="her_icons afar"
             src={"/images/o.png"}
             width={40}
             height={40}
             alt="icons"
           />
           <Image
-            className="her_icons"
+            className="her_icons shan"
             src={"/images/line.png"}
             width={40}
             height={40}
@@ -53,12 +50,56 @@ const Rate = () => {
           />
 
           <Image
-            className="her_icons"
+            className="her_icons lix"
             src={"/images/x2.png"}
             width={40}
             height={40}
             alt="icons"
           />
+          <div className="text_hero">
+            <h2 className="rate_title">
+              Qiimee shabakada E-zako
+              <br />
+              Jawaabaheeda & qaabkeeda
+            </h2>
+          </div>
+          {!rate && (
+            <button type="button" onClick={Ratechange}>
+              <i className="fa-solid fa-star-half-stroke"></i> Qiimee
+            </button>
+          )}
+          {rate && (
+            <div className="input_rate">
+              <div className="xidigo">
+                <input type="radio" name="rate" id="rate1" />
+                <label htmlFor="rate1">
+                  <i className="fa-solid fa-star"></i>
+                </label>
+                <input type="radio" name="rate" id="rate2" />
+                <label htmlFor="rate2">
+                  <i className="fa-solid fa-star"></i>
+                </label>
+                <input type="radio" name="rate" id="rate3" />
+                <label htmlFor="rate3">
+                  <i className="fa-solid fa-star"></i>
+                </label>
+                <input type="radio" name="rate" id="rate4" />
+                <label htmlFor="rate4">
+                  <i className="fa-solid fa-star"></i>
+                </label>
+                <input type="radio" name="rate" id="rate5" />
+                <label htmlFor="rate5">
+                  <i className="fa-solid fa-star"></i>
+                </label>
+              </div>
+              <form action="">
+                <textarea name="comment" maxLength={120} />
+                <button type="submit">
+                  <i className="fa-solid fa-paper-plane"></i> Dir Qiimaynta
+                </button>
+              </form>
+            </div>
+          )}
         </div>
       </div>
     </>
