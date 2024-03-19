cae1a57d05ba3dc990c43e8ccb29084449a4edc6
abu-amar jumhuur <jumhuur123@hotmail.com>
2024-03-19 06:12:16 +0300

    done animate

--- src/components/ModejTabaruc.tsx
+++ src/components/ModejTabaruc.tsx
@@ -5,16 +5,18 @@ const ModelJawaab = () => {
   const { activeModelTabaruc, ModelT } = Usedata();
   return (
     <>
-      <motion.div
-        initial={{ y: "2rem", opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 2, type: "spring" }}
-        className={!ModelT ? "ModelJawaab" : "ModelJawaab active"}
-      >
-        <div className="meesha_jawaabta">
-          <p>Model Tabaruc</p>
-        </div>
-      </motion.div>
+      {ModelT && (
+        <motion.div
+          initial={{ y: "2rem", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 2, type: "spring" }}
+          className={"ModelJawaab active"}
+        >
+          <div className="meesha_jawaabta">
+            <p>Model Tabaruc</p>
+          </div>
+        </motion.div>
+      )}
     </>
   );
 };
