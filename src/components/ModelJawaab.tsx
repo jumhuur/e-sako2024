077297a8ee28e47da738faff10fdd9f9d0445594
abu-amar jumhuur <jumhuur123@hotmail.com>
2024-03-19 18:48:20 +0300

    done

--- src/components/ModelJawaab.tsx
+++ src/components/ModelJawaab.tsx
@@ -1,15 +1,24 @@
 "use client";
 import { Usedata } from "@/context/XisaabContex";
 import JawaabSako from "./Jawaab";
-
+import { motion } from "framer-motion";
 const ModelJawaab = () => {
   const { Jawaab } = Usedata();
   return (
     <>
       <div className={!Jawaab ? "ModelJawaab" : "ModelJawaab active"}>
-        <div className="meesha_jawaabta">
-          <JawaabSako />
-        </div>
+        <>
+          {Jawaab && (
+            <motion.div
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 2, type: "spring" }}
+              className="meesha_jawaabta"
+            >
+              <JawaabSako />
+            </motion.div>
+          )}
+        </>
       </div>
     </>
   );
